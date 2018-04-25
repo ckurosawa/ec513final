bdf57e10bb1d600a28bb8cfa225d1d05ba4366f5
ckurosawa <38536651+ckurosawa@users.noreply.github.com>
2018-04-25 15:47:39 -0400

    Add files via upload

--- dispatch_interface.v
+++ dispatch_interface.v
@@ -6,42 +6,34 @@ module dispatch_interface #(parameter CORE = 0, DATA_WIDTH = 32, INDEX_WIDTH = 8
   write,
   in_address,
   in_data,
-  in_valid,
   out_address,
   out_data,
   report  
 );
-localparam OVERHEAD = VALID_BIT;
 
 input clock;
 input reset;
 
 input write;
-input in_valid;
 input[INDEX_WIDTH - 1:0] in_address;
 input[DATA_WIDTH - 1:0] in_data;
 
 input read;
-output[INDEX_WIDTH - 1:0] out_address;
-output[DATA_WIDTH + OVERHEAD - 1:0] out_data;
+input[INDEX_WIDTH - 1:0] out_address;
+output[DATA_WIDTH - 1:0] out_data;
 
 input report;
 
-wire[DATA_WIDTH + OVERHEAD - 1:0] in_databus; //Combine overhead w/ data in sram
-
-assign in_databus[DATA_WIDTH + OVERHEAD - 1: DATA_WIDTH + OVERHEAD - VALID_BIT - 1] = in_valid;
-assign in_databus[DATA_WIDTH + OVERHEAD - VALID_BIT - 1:0] =  in_data;
-
-BSRAM #(CORE, DATA_WIDTH + OVERHEAD, INDEX_WIDTH) DIS_RAM(
+BSRAM #(CORE, DATA_WIDTH, INDEX_WIDTH) DISP_RAM(
   .clock(clock),
   .reset(reset),
   .readEnable(read),
-  .readAddress(in_address),
-  .readData(in_databus),
+  .readAddress(out_address),
+  .readData(out_data),
   
   .writeEnable(write),
-  .writeAddress(out_address),
-  .writeData(out_data),
+  .writeAddress(in_address),
+  .writeData(in_data),
   
   .report(report)
 );
@@ -58,7 +50,6 @@ always @ (posedge clock) begin
     $display ("| Write             [%b]", write);
     $display ("| Out Data          [%h]", out_data);
     $display ("| In Data           [%h]", in_data);
-    $display ("| Valid             [%b]", valid);
     $display ("----------------------------------------------------------------------");
   end
 end
