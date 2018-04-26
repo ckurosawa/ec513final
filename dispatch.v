d6e3f10b66172e1aaf6466fca643a22e108ba226
ckurosawa <38536651+ckurosawa@users.noreply.github.com>
2018-04-26 17:31:32 -0400

    Add files via upload

--- dispatch.v
+++ dispatch.v
@@ -1,4 +1,4 @@
-module dispatch_unit #(parameter CORE = 0, DATA_WIDTH = 32, INDEX_WIDTH = 8)(
+module dispatch_unit #(parameter CORE = 0, DATA_WIDTH = 32, INDEX_WIDTH = 3)(
   clock,
   reset,
   report,
@@ -22,24 +22,28 @@ module dispatch_unit #(parameter CORE = 0, DATA_WIDTH = 32, INDEX_WIDTH = 8)(
 reg[31:0] queue_top;
 reg[31:0] queue_bot;
 
+wire[4:0] ins_dest;
+wire[4:0] ins_src1;
+wire[4:0] ins_src2;
+
 //Instruction status
 //FORMAT:[ISSUE:READ:EX:WB]  -- index corresponds to depth
 reg[3:0] ins_stat[7:0];
 
 //ID status
-//FORMAT: [OPCODE(7):ID_DEST(5):ID_SRC1(5):ID_SRC2(5)]
+//FORMAT: [OPCODE(7):ID_DEST(5)]
 reg[11:0] id_stat;
 
-//EXE status
-//FORMAT: [OPCODE(7):EXE_DEST(5):EXE_SRC1(5):EXE_SRC2(5)]
-reg[11:0] exe_stat; //only 1 functional unit
+//EXE status (only 1 functional unit)
+//FORMAT: [OPCODE(7):EXE_DEST(5)]
+reg[11:0] exe_stat;
 
 //MEM status
-//FORMAT: [OPCODE(7):MEM_DEST(5):MEM_SRC1(5):MEM_SRC2(5)]
+//FORMAT: [OPCODE(7):MEM_DEST(5)]
 reg[11:0] mem_stat;
 
 //WB status
-//FORMAT: [OPCODE(7):WB_DEST(5):WB_SRC1(5):MEM_SRC2(5)]
+//FORMAT: [OPCODE(7):WB_DEST(5)]
 reg[11:0] wb_stat;
 
 //Holds instructions (sequential)
@@ -57,6 +61,24 @@ dispatch_interface disp_int #(parameter CORE, DATA_WIDTH, INDEX_WIDTH)(
 );
 
 always @ (posedge clock) begin
-  
+  if (~reset) begin
+    id_stat[11:5] <= id_opcode;
+    id_stat[4:0] <= id_dest;
+    exe_stat[11:5] <= exe_opcode;
+    exe_stat[4:0] <= exe_dest;
+    mem_stat[11:5] <= mem_opcode;
+    mem_stat[4:0] <= mem_dest;
+    wb_stat[11:5] <= wb_opcode;
+    wb_stat[4:0] <= wb_dest;
+    
+  else if (reset) begin
+    id_stat <= 0;
+    exe_stat <= 0;
+    mem_stat <= 0;
+    wb_stat <= 0;
+    queue_top <= 0;
+    queue_bot <= 1;
+  end
+end
 
 endmodule
